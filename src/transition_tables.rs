5725ee956fa9caf1450fecbbb3b93d08fa1a6d75
Sarang Hadagali <sarang.hadagali@gmail.com>
2023-11-10 15:05:09 -0500

    adding transition table structs

--- src/transition_tables.rs
+++ src/transition_tables.rs
@@ -1,36 +1,36 @@
-//#[derive(Debug)]
-//pub struct TransitionTable {
-//    alphabet: Vec<char>,
-//    state: Vec<String>,
-//    initial: String,
-//    accepting: Vec<String>,
-//    transitions: Vec<Transition>
-//}
-//
-//impl TransitionTable {
-//    pub fn new() -> Self {
-//        Self {
-//            alphabet: Vec::new(),
-//            state: Vec::new(),
-//            initial: String::new(),
-//            accepting: Vec::new(),
-//            transitions: Vec::new()
-//        }
-//    }
-//
-//    pub fn validate(&self) -> Result<(), String> {
-//        // Pass through TransitionTable and validate all fields
-//        // Return specific error message
-//        todo!();
-//    }
-//}
-//
-//
-//#[derive(Debug)]
-//pub struct Transition {
-//    from: String,
-//    to: String,
-//    symbol: char
-//}
-//
-//
+#[derive(Debug)]
+pub struct TransitionTable {
+    alphabet: Vec<char>,
+    state: Vec<String>,
+    initial: String,
+    accepting: Vec<String>,
+    transitions: Vec<Transition>
+}
+
+impl TransitionTable {
+    pub fn new() -> Self {
+        Self {
+            alphabet: Vec::new(),
+            state: Vec::new(),
+            initial: String::new(),
+            accepting: Vec::new(),
+            transitions: Vec::new()
+        }
+    }
+
+    pub fn validate(&self) -> Result<(), String> {
+        // Pass through TransitionTable and validate all fields
+        // Return specific error message
+        todo!();
+    }
+}
+
+
+#[derive(Debug)]
+pub struct Transition {
+    from: String,
+    to: String,
+    symbol: char
+}
+
+
