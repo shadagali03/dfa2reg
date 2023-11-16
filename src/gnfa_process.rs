8fff074df2444c19c1f086581b201383067b48d4
Sarang Hadagali <sarang.hadagali@gmail.com>
2023-11-16 11:12:40 -0500

    finished minimum transition function, has a lot of optimization issues but will work for now

--- src/gnfa_process.rs
+++ src/gnfa_process.rs
@@ -31,28 +31,48 @@ fn create_finish_state(input_table: &mut TransitionTable) -> Result<(), String> 
     Ok(())
 }
 
+// ISSUES WITH CURRENT CODE
+// - Does not acccount for self loops -> should not be counted
+// - Does not account for multiple symbols going towards same node, will be counted as 2 seperate
+// edges
 // Pick the state that will minimize the number of transitions
 // Number of transitions is equal to # of transitions incoming x # of transitions outgoing
-fn find_minimum_transitions_state(input_table: &mut TransitionTable) -> &str {
+fn find_minimum_transitions_state(input_table: &mut TransitionTable) -> String {
     // Iterate through avaliable states
     // Keep a Hashmap key -> state, value -> (# incoming, # outgoing)
 
-    let mut track_indegrees = HashMap::<String, (i64, i64)>::new();
-    for (pair, _can_transition_to) in input_table.delta_transitions.iter() {
+    let mut track_edges = HashMap::<String, (i64, i64)>::new();
+    for state in input_table.states.iter() {
+        track_edges.insert(state.to_string(), (0, 0));
+    }
+    for (pair, can_transition_to) in input_table.delta_transitions.iter() {
         let (from, _symbol) = pair;
 
-        if let Some(pair) = track_indegrees.get_mut(from) {
-            pair.0 += 1
-        } else {
-            track_indegrees.insert(from.to_string(), (0, 0));
+        // Handles outging edgese
+        if let Some(pair) = track_edges.get_mut(from) {
+            pair.1 += can_transition_to.len() as i64;
+        }
+
+        for go_to in can_transition_to.iter() {
+            if go_to != from {
+                if let Some(pair) = track_edges.get_mut(go_to) {
+                    pair.0 += 1;
+                }
+            }
         }
     }
 
-    println!("{:?}", track_indegrees);
+    let mut min_state: (i64, String) = (std::i64::MAX, String::new());
+    for (state, pair) in track_edges.iter() {
+        if pair.0 * pair.1 < min_state.0 {
+            min_state = (pair.0 * pair.1, state.to_string());
+        }
+    }
 
-    ""
+    min_state.1
 }
-fn _rip_states(_input_table: &mut TransitionTable, _to_rip: &String) {
+
+fn _rip_state(_input_table: &mut TransitionTable, _to_rip: &String) {
     todo!();
 }
 
@@ -67,7 +87,9 @@ pub fn run_gnfa(input_table: &mut TransitionTable) -> Result<(), String> {
         Err(msg) => return Err(msg.to_string()),
     }
 
-    let _state_to_rip = find_minimum_transitions_state(input_table);
+    let state_to_rip = find_minimum_transitions_state(input_table);
+
+    println!("State to rip: {state_to_rip}");
 
     println!("{input_table}");
 
